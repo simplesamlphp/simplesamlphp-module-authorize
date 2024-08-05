5bdf350f06ab409dae03b302589428b9f1d9e311
dbollaer <1390812+dbollaer@users.noreply.github.com>
2024-08-05 23:55:35 +0200

    feat(reauthenticate): Add flag to allow re-authentication when user is unauthorized. Resolves #17

---
+++ tests/src/Controller/AuthorizeAllowReAuthenticateOnUnauthorizedTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\Module\authorize\Controller;
+
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Auth;
+use SimpleSAML\Configuration;
+use SimpleSAML\Module\authorize\Controller;
+use SimpleSAML\Session;
+use SimpleSAML\XHTML\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Set of tests for the controllers in the "authorize" module.
+ *
+ * @package SimpleSAML\Test
+ */
+class AuthorizeAllowReAuthenticateOnUnauthorizedTest extends TestCase
+{
+    /** @psalm-suppress PropertyNotSetInConstructor */
+    protected Configuration $config;
+
+    /** @psalm-suppress PropertyNotSetInConstructor */
+    protected string $stateId;
+
+
+    /**
+     * Set up for each test.
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->config = Configuration::loadFromArray(
+            [
+                'baseurlpath' => 'https://example.org/simplesaml',
+                'module.enable' => ['authorize' => true],
+            ],
+            '[ARRAY]',
+            'simplesaml',
+        );
+
+        $state = [
+            'StateId' => 'SomeState',
+            'Source' => ['auth' => 'test'],
+            'authprocAuthorize_reject_msg' => 'Test Rejected',
+            'authprocAuthorize_error_url' => true,
+            'authprocAuthorize_ctx' => 'example',
+            'authprocAuthorize_allow_re_authenticate_on_unauthorized' => true,
+
+        ];
+        $this->stateId = Auth\State::saveState($state, 'authorize:Authorize');
+
+        Configuration::setPreLoadedConfig($this->config, 'config.php');
+    }
+
+
+    /**
+     * Test that a valid requests results in a HTTP/403 Forbidden page with translated messages
+     * @return void
+     * @throws \Exception
+     */
+    public function testValidRequest()
+    {
+        $request = Request::create(
+            '/',
+            'GET',
+            ['StateId' => $this->stateId],
+        );
+        $session = Session::getSessionFromRequest();
+
+        $c = new Controller\Authorize($this->config, $session);
+
+        /** @var \SimpleSAML\XHTML\Template $response */
+        $response = $c->forbidden($request);
+
+        $this->assertInstanceOf(Template::class, $response);
+        $this->assertTrue($response->isForbidden());
+    }
+}
